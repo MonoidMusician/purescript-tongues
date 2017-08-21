b39f60f57093be215f3074d6b8020d383d39ab33
Nicholas Scheel <monoidmusician@gmail.com>
2017-08-20 21:43:09 -0500

    Add documentation

---
+++ README.md
@@ -0,0 +1,46 @@
+## Background
+Operators are great! We love them. But sometimes you just need to apply one argument to them. That's great too.
+
+Functions are awesome at partial application, and judicious use of `flip` and `flap` (`<@>`) makes for more fun.
+
+For operators, we can use sections to apply either left _or_ right values. In PureScript the syntax is a slight bit more verbose, but no matter: `(a <> _)` to apply a left argument, and `(_ <> b)` to apply a right argument.
+
+PureScript also requires that these operators all have names (useful when generating readable code): `<>` is called `append`!
+
+The way these methods are defined is designed for using them like `append a b`, where `a` is ostensibly the thing on the left, and `b` goes on the right.
+
+## The Problem
+But then partial application happens and suddenly `append x` no longer _appends_ to a semigroup, but _prepends_ `x` to the next value passed in!
+
+**This is ridiculous.**
+
+## The Solution
+So I created a module that rights this wrong by lefting all the rights. With the functions in this module, `append b` will really mean that `b` gets appended; the old `append` is now `prepend`, since that is what that means.
+
+Thus, most functions in this module are simply flipped versions of the methods provided in Prelude.
+
+Some others have had prepositions added to their names to make better sense in English.
+
+All suggestions and pull requests are welcome!
+
+## Rule of Thumb
+This is confusing, but so is English. So if you get confused, ask yourself how many arguments you are going to be giving to the function at once? This will determine which version will make the most sense:
+1. Perfect, use this library!
+2. Cool, just use the regular version
+
+In order for you and grokkers of your code to be able to readily distinguish the functions, a qualified import is recommended.
+
+Note that signatures in the source use extra parentheses to remind you that they are best used as partially applied functions!
+
+## Examples
+```purescript
+import Data.Functor (map)
+import Tongues.Sections (append, prepend) as Section
+
+map (Section.append " ") ["Hello", "World"] == ["Hello ", "World "]
+map (Section.prepend " ") ["Hello", "World"] == [" Hello", " World"]
+
+mkQualified modules reference =
+  Section.append reference $ foldMap (Section.append ".") modules
+mkQualified ["Tongues", "Sections"] "append" == "Tongues.Sections.append"
+```
--- src/Tongues.purs
+++ src/Tongues.purs
@@ -8,48 +8,86 @@ import Data.Ord as Data.Ord
 import Control.Semigroupoid as Control.Semigroupoid
 import Data.Function as Data.Function
 import Data.Functor as Data.Functor
-import Data.Function (flip)
+import Data.Function (flip, id)
 
+-- | `prepend a == (a <> _)`
 prepend :: forall a. Data.Semigroup.Semigroup a => a -> (a -> a)
 prepend = Data.Semigroup.append
 
+-- | `append b == (_ <> b)`
 append :: forall a. Data.Semigroup.Semigroup a => a -> (a -> a)
 append = flip prepend
 
+-- | `addTo a == (a + _)`
 addTo :: forall a. Data.Semiring.Semiring a => a -> (a -> a)
 addTo = Data.Semiring.add
 
+-- | FIXME: how should English for non-commutative semirings???
+-- | `multiplyBy `
 multiplyBy :: forall a. Data.Semiring.Semiring a => a -> (a -> a)
 multiplyBy = Data.Semiring.mul
 
+-- | `subtractFrom a == (a - _)`
 subtractFrom :: forall a. Data.Ring.Ring a => a -> (a -> a)
 subtractFrom = Data.Ring.sub
 
+-- | `minus b == (_ - b)`
 minus :: forall a. Data.Ring.Ring a => a -> (a -> a)
 minus = flip subtractFrom
 
--- divideBy 5 = (_/5)
+-- `divideBy b = (_/b)`
 divideBy :: forall a. Data.EuclideanRing.EuclideanRing a => a -> (a -> a)
 divideBy = Data.EuclideanRing.div
 
+-- | `compareTo a == (a `compare` _)`
+-- |
+-- | Example:
+-- | ```purescript
 -- | sgn = compareTo zero
 -- | sgn one = GT
 -- | sgn zero = EQ
 -- | sgn (negate one) = LT
+-- | ```
 compareTo :: forall a. Data.Ord.Ord a => a -> (a -> Data.Ord.Ordering)
 compareTo = flip Data.Ord.compare
 
-composeAfter :: forall b c d a. Control.Semigroupoid.Semigroupoid a => a c d -> (a b c -> a b d)
-composeAfter = Control.Semigroupoid.compose
+-- | `lessThan b == (_ < b) == (b > _)`
+lessThan :: forall a. Data.Ord.Ord a => a -> (a -> Boolean)
+lessThan = Data.Ord.greaterThan
 
+-- | `greaterThan b == (_ > b) == (b < _)`
+greaterThan :: forall a. Data.Ord.Ord a => a -> (a -> Boolean)
+greaterThan = Data.Ord.lessThan
+
+-- | `lessThanOrEq b == (_ <= b) == (b >= _)`
+lessThanOrEq :: forall a. Data.Ord.Ord a => a -> (a -> Boolean)
+lessThanOrEq = Data.Ord.greaterThanOrEq
+
+-- | `greaterThanOrEq b == (_ >= b) == (b <= _)`
+greaterThanOrEq :: forall a. Data.Ord.Ord a => a -> (a -> Boolean)
+greaterThanOrEq = Data.Ord.lessThanOrEq
+
+-- | `noLessThan a == \b -> if b < a then a else b == max a`
+-- | Alternative names welcome.
+noLessThan :: forall a. Data.Ord.Ord a => a -> (a -> a)
+noLessThan = Data.Ord.max
+
+-- | `noGreaterThan a == \b -> if b > a then a else b == min a`
+noGreaterThan :: forall a. Data.Ord.Ord a => a -> (a -> a)
+noGreaterThan = Data.Ord.min
+
+-- | `composeBefore a == (a <<< _) == (_ >>> a)`
 composeBefore :: forall b c d a. Control.Semigroupoid.Semigroupoid a => a b c -> (a c d -> a b d)
 composeBefore = flip composeAfter
 
-applyTo :: forall a b. (a -> b) -> (a -> b)
-applyTo = Data.Function.apply
+-- | `composeAfter b == (_ <<< b) == (b >>> _)`
+composeAfter :: forall b c d a. Control.Semigroupoid.Semigroupoid a => a c d -> (a b c -> a b d)
+composeAfter = Control.Semigroupoid.compose
 
-applyValue :: forall a b. a -> ((a -> b) -> b)
-applyValue = flip applyTo
+-- | `applyTo x == (_ $ x)`
+applyTo :: forall a b. a -> ((a -> b) -> b)
+applyTo = flip id
 
+-- | `mapTo x == (_ $> x) == (x <$ _) == (const x <$> _) == (_ <#> const x)`
 mapTo :: forall f a b. Data.Functor.Functor f => b -> (f a -> f b)
 mapTo = Data.Functor.voidRight
